5138f738756e39e1b3b5b25fe4484d96aabe9a0e
nammuSeb <62795251+nammuSeb@users.noreply.github.com>
2024-11-15 23:17:53 +0100

    15.11.2024

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const upload = multer({ storage: storage });
 
 app.use(cors({
     //origin: 'http://localhost:5173',
-    //origin: 'https://www.inkigai.ch',
-    origin: 'http://ikigai.jcloud.ik-server.com/',
+    origin: 'https://www.inkigai.ch',
+    //origin: 'http://ikigai.jcloud.ik-server.com/',
 }));
 
 app.use(express.json());
